7bb1e3841df9b546d65fc0162c5ddac440506351
ammar <ammar.ali.haidar.1990@gmail.com>
2024-02-21 18:40:36 +0400

    start

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <div className="flex items-center justify-center h-screen">Raya... is coming</div>
+    <div className="flex items-center justify-center h-screen bg-slate-950">
+      <span className="px-4 py-2 rounded-full  bg-indigo-500/20 animate-pulse text-white">
+        Raya... is coming
+      </span>
+    </div>
   );
 }
